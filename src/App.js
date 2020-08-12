296553278b9bdbca92563f30ae8fd8d883946f84
Chamiru <chamisenarath@gmail.com>
2020-08-12 10:51:47 +0530

    Initial commit
    Axios React HTTP request

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import './App.css';
+
+import UserForm from './Components/UserForm'
+
+class App extends Component {
+  state = {
+    repos: null
+  }
+
+  getUser = (e) => {
+    e.preventDefault();
+    const user = e.target.elements.username.value;
+    if(user){
+      axios.get(`https://api.github.com/users/${user}`)
+      .then((res) => {
+        
+        const repos = res.data.public_repos;
+        this.setState({repos});
+      })
+    }
+    else return;
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">
+            HTTP calls in React
+          </h1>
+        </header>
+        <UserForm getUser={this.getUser} />
+        { this.state.repos ? <p>Number: {this.state.repos}</p>:
+        <p>Please enter a usernmae.</p>}
+      </div>
+    )
+  }
+}
+
+export default App;
